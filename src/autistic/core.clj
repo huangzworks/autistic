29599d5bfc7a6147739bdd42c07529ba8967d729
huangz1990 <huangz1990@gmail.com>
2012-11-22 23:31:34 +0800

    init project

---
+++ src/autistic/core.clj
@@ -0,0 +1,1 @@
+(ns autistic.core)
---
+++ test/autistic/test/core.clj
@@ -0,0 +1,6 @@
+(ns autistic.test.core
+  (:use [autistic.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
